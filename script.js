68df14eaf2f6714eae036cb0b7ae304ab3b034a7
Yash Gopi Waghadhare <127091514+yashwaghadhare@users.noreply.github.com>
2023-05-14 02:56:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Database Designer", "Web development"],
+        strings: ["Database Designer", "Web Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Database Designer", "Web development"],
+        strings: ["Database Designer", "Web Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
